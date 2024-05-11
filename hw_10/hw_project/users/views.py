1aa2687347037d4180f7f053ac1ddc84621a10ee
YarValeriy <147444561+YarValeriy@users.noreply.github.com>
2024-05-11 11:33:09 +0300

    Add files via upload

---
+++ hw_10/hw_project/users/views.py
@@ -0,0 +1,63 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from .forms import RegisterForm, LoginForm
+
+
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quotes:root')
+        # return redirect('/')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+            # return redirect('/')
+        else:
+            return render(request, 'users/signup.html', context={"form": form})
+
+    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quotes:root')
+        # return redirect('/')  # Redirect to home page after successful login
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='quotes:root')
+        # return redirect('/')  # Redirect to home page after successful login
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+
+@login_required(login_url='quotes:root')
+def logoutuser(request):
+    logout(request)
+    return redirect(to='quotes:root')
+    # return redirect('/')  # Redirect to home page after logout
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
